580f1f1dfc3aa3e7012fd42a70c5d5ec9e7bea82
codelogicws <russ@codelogic.ws>
2016-04-10 12:32:48 -0700

    First Commit

---
+++ Intermediate Algos/RomanNumerals/test.js
@@ -0,0 +1,22 @@
+var romanNumerals = {
+    1000 : 'M',
+    500 : 'D',
+    100 : 'C',
+    50 : 'L',
+    10 : 'X',
+    5 : 'V',
+    1 : 'I'
+}
+
+function convertToRoman(num) {
+    var  = closestTo(num);
+    return num;
+}
+
+function run(){
+    test(3);
+}
+
+function test(num){
+    console.log( convertToRoman(num) + '  from  ' + num);
+}
