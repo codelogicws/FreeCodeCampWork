e6cdea2f4c7a9864022b9069b5d10e30d7c90f25
codelogicws <russ@codelogic.ws>
2016-04-10 18:08:09 -0700

    Better way to declair the roman nums. This way its one place to rename the naming convention.

--- Intermediate Algos/RomanNumerals/test.js
+++ Intermediate Algos/RomanNumerals/test.js
@@ -1,11 +1,14 @@
-var romanNumerals = {
-    1000 : 'M',
-    500 : 'D',
-    100 : 'C',
-    50 : 'L',
-    10 : 'X',
-    5 : 'V',
-    1 : 'I'
+var romanNumerals = [];
+addToRomanNumeralLookUp(1000, 'M');
+addToRomanNumeralLookUp(500,  'D');
+addToRomanNumeralLookUp(100,  'C');
+addToRomanNumeralLookUp(50,   'L');
+addToRomanNumeralLookUp(10,   'X');
+addToRomanNumeralLookUp(5,    'V');
+addToRomanNumeralLookUp(1,    'I');
+
+function addToRomanNumeralLookUp(number, letter){
+    romanNumerals.push({'number': number, 'letter': letter});
 }
 
 function convertToRoman(num) {
