c3d3c58d3577b830a9c1f2e5c2e122f19221a5bf
codelogicws <russ@codelogic.ws>
2016-04-10 21:13:08 -0700

    Using a better solution I found online. I solved the solution on my own with some bugs and now I'm messing with someone elses

--- Intermediate Algos/RomanNumerals/test.js
+++ Intermediate Algos/RomanNumerals/test.js
@@ -1,60 +1,26 @@
-var romanNumerals = [];
-addToRomanNumeralLookUp(1000, 'M');
-addToRomanNumeralLookUp(500,  'D');
-addToRomanNumeralLookUp(100,  'C');
-addToRomanNumeralLookUp(50,   'L');
-addToRomanNumeralLookUp(10,   'X');
-addToRomanNumeralLookUp(5,    'V');
-addToRomanNumeralLookUp(1,    'I');
-
-function addToRomanNumeralLookUp(number, letter){
-    romanNumerals.push({'number': number, 'letter': letter});
-}
-
-function convertToRoman(num) {
-
-    var romanNumber = '';
-    var tens = 1000;
-    while(tens >= 1){
-        var x = Math.floor(num/tens);
-        romanNumber += getRoman(x * tens);
-        num %= tens;
-        tens /= 10;
-    }
-
-    return romanNumber;
-}
-
-function getRoman(num){
-    if(num <= 0)
-        return '';
-    var close = romanNumerals.reduce(closestRoman(num));
-    if(close.number < num)
-        return close.letter + getRoman(num - close.number);
-    return getRoman(close.number - num) + close.letter;
-}
-
-function closestRoman(num){
-    return function(lastRoman, newRoman){
-        if(lastRoman == null)
-            return newRoman;
-        if(  difference(lastRoman.number, num) < difference(newRoman.number, num)  )
-            return lastRoman;
-        return newRoman;
-    }
-}
-
-function difference(num1, num2){
-    return Math.abs(num1 - num2);
+function romanize (num) {
+    if (!+num)
+        return false;
+    console.log( String(+num).split("") );
+    var digits = String(+num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+            "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+            "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
 }
 
 function run(){
     for (var i=0;i<16;i++){
         test(i);
     }
+
     test(3584);
 }
 
 function test(num){
-    console.log( convertToRoman(num) + '  from  ' + num);
+    console.log( romanize(num) + '  from  ' + num);
 }
