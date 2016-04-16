89df52b156aafb2219f44e114e7a16676addf1a8
codelogicws <russ@codelogic.ws>
2016-04-15 20:45:51 -0700

    Getting more done with roman num

--- Intermediate Algos/RomanNumerals/test.js
+++ Intermediate Algos/RomanNumerals/test.js
@@ -1,58 +1,34 @@
-var romanNumerals = [];
-addToRomanNumeralLookUp(1000, 'M');
-addToRomanNumeralLookUp(500,  'D');
-addToRomanNumeralLookUp(100,  'C');
-addToRomanNumeralLookUp(50,   'L');
-addToRomanNumeralLookUp(10,   'X');
-addToRomanNumeralLookUp(5,    'V');
-addToRomanNumeralLookUp(1,    'I');
+var romanList = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "X"];
 
-function addToRomanNumeralLookUp(number, letter){
-    romanNumerals.push({'number': number, 'letter': letter});
-}
-
-function convertToRoman(num) {
-
-    var romanNumber = '';
-    var tens = 1000;
-    while(tens >= 1){
-        var x = Math.floor(num/tens);
-        romanNumber += getRoman(x * tens);
-        num %= tens;
-        tens /= 10;
+function convertToRoman(num){
+    var eachDigit = String(num).split('');
+    var multiplier = Math.pow(10, eachDigit.length-1);
+    var roman = "";
+    while(multiplier > 1){
+        roman += eachDigit.shift();
+        multiplier = multiplier/10;
     }
-
-    return romanNumber;
+    roman += romanList[eachDigit.pop()];
+    return roman;
 }
 
-function getRoman(num){
-    if(num <= 0)
-        return '';
-    var close = romanNumerals.reduce(closestRoman(num));
-    if(close.number < num)
-        return close.letter + getRoman(num - close.number);
-    return getRoman(close.number - num) + close.letter;
-}
 
-function closestRoman(num){
-    return function(lastRoman, newRoman){
-        if(lastRoman == null)
-            return newRoman;
-        if(  difference(lastRoman.number, num) < difference(newRoman.number, num)  )
-            return lastRoman;
-        return newRoman;
-    }
-}
 
-function difference(num1, num2){
-    return Math.abs(num1 - num2);
-}
+
+
+
+
+
+
+
+
+
 
 function run(){
-    for (var i=0;i<16;i++){
-        test(i);
-    }
-    test(3584);
+    //for (var i=0;i<16;i++){
+    //    test(i);
+    //}
+    test(216516);
 }
 
 function test(num){
