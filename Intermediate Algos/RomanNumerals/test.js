8e6dda30f88e20b55e8357438e511fcdad079234
codelogicws <russ@codelogic.ws>
2016-04-16 22:39:28 -0700

    About to try something risky

--- Intermediate Algos/RomanNumerals/test.js
+++ Intermediate Algos/RomanNumerals/test.js
@@ -1,7 +1,7 @@
 var romanList = [
     ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-    ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
-    ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
 ];
 
 //Roman Numerals
@@ -12,17 +12,13 @@ var romanList = [
 
 function convertToRoman(num){
     var eachDigit = String(num).split('');
-    var multiplier = Math.pow(10, eachDigit.length-1);
-    var roman = "";
-    while(multiplier > 1){
+    var roman = '';
+    var maxDigitsUnder1000 = (eachDigit.length > 3)? 3 : eachDigit.length;
+    for(var i=maxDigitsUnder1000; i > 0;i--){
+        roman += romanList[i-1][eachDigit[maxDigitsUnder1000-i]];
         debugger;
-        currentDigit = eachDigit.shift();
-        if(multiplier == 10)
-            debugger;
-        roman += currentDigit;
-        multiplier = multiplier/10;
     }
-    roman += romanList[eachDigit.pop()];
+    //roman = 'M'.repeat() + roman;
     return roman;
 }
 
@@ -33,7 +29,7 @@ function run(){
     //for (var i=0;i<16;i++){
     //    test(i);
     //}
-    test(216516);
+    test(6516);
 }
 
 function test(num){
