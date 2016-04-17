2f0ce29645755acf0f89080bfa5400c53f957d20
codelogicws <russ@codelogic.ws>
2016-04-16 19:28:44 -0700

    whereAreYou is working.

--- Intermediate Algos/Where art thou/test.js
+++ Intermediate Algos/Where art thou/test.js
@@ -4,23 +4,19 @@ function whereAreYou(collection, source) {
     // What's in a name?
     var arr = [];
     // Only change code below this line
-    //hasOwnProperty
 
     collection.forEach(function(queryTerms){
-        var keys = Object.keys(queryTerms);
+        var keys = Object.keys(source);
+        var match = true;
         for(var key in keys){
-            var sourceKeys = Object.keys(source);
-            var collProperty = keys[key];
-            if( sourceKeys.indexOf(collProperty) > -1 && queryTerms[collProperty] === source[collProperty] ){
-                arr.push(queryTerms);
-
-            }
+            var sourceKey = keys[key];
+            if( !(queryTerms[sourceKey] === source[sourceKey]) )
+                match = false;
         }
+        if(match)
+            arr.push(queryTerms);
     });
-
-
     return arr;
-
 }
 
 function run() {
