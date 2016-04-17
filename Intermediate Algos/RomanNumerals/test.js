54b855124ba98976c1baee8dc234794db14aa908
codelogicws <russ@codelogic.ws>
2016-04-16 21:29:05 -0700

    About to try something risky

--- Intermediate Algos/RomanNumerals/test.js
+++ Intermediate Algos/RomanNumerals/test.js
@@ -1,11 +1,25 @@
-var romanList = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "X"];
-        i = 3;
+var romanList = [
+    ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+];
+
+//Roman Numerals
+//Array.splice()
+//Array.indexOf()
+//Array.join()
+
+
 function convertToRoman(num){
     var eachDigit = String(num).split('');
     var multiplier = Math.pow(10, eachDigit.length-1);
     var roman = "";
     while(multiplier > 1){
-        roman += eachDigit.shift();
+        debugger;
+        currentDigit = eachDigit.shift();
+        if(multiplier == 10)
+            debugger;
+        roman += currentDigit;
         multiplier = multiplier/10;
     }
     roman += romanList[eachDigit.pop()];
@@ -23,5 +37,5 @@ function run(){
 }
 
 function test(num){
-    console.log( romanize(num) + '  from  ' + num);
+    console.log( convertToRoman(num) + '  from  ' + num);
 }
