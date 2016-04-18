9fbf591e77a38d8dd406fb6f6cfec6e2498088d2
codelogicws <russ@codelogic.ws>
2016-04-17 18:52:16 -0700

    Finished the "Missing Letters" assignment

--- Intermediate Algos/Missing letters/test.js
+++ Intermediate Algos/Missing letters/test.js
@@ -1,6 +1,14 @@
+function fearNotLetter(str) {
+    for(var i=1;i<str.length;i++){
+        if(str.charCodeAt(i)-str.charCodeAt(i-1)>1)
+            return String.fromCharCode(str.charCodeAt(i)-1);
+    }
+    return undefined;
+}
 
 
 function run(){
+    var test1 = fearNotLetter("abce");
     debugger;
 }
 
