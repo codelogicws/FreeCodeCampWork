ca2d0f567e6b16c35910f8f08cd3d13ea1fa2e37
codelogicws <russ@codelogic.ws>
2016-04-17 20:25:42 -0700

    Improved on the "Sorted Union" assignment

--- Intermediate Algos/Sorted Union/test.js
+++ Intermediate Algos/Sorted Union/test.js
@@ -1,21 +1,21 @@
-//Arguments object
-//Array.reduce()
 function uniteUnique(arr1, arr2, arr3) {
 
-    var argsAsArray = Array.prototype.slice.call(arguments);
+    var newArray = [];
+    for(var i=0;i<arguments.length;i++){
+        newArray = newArray.concat(arguments[i]);
+    }
 
-    var returnArray = argsAsArray.reduce(function(arraySoFar, inputArray){
-        inputArray.reduce(function(arraySoFar, element){
-
-            if(arraySoFar.indexOf(element) == -1)
+    var returnArray = newArray.reduce(function(arraySoFar, element){
+            if( notInArray(arraySoFar, element) )
                 arraySoFar.push(element);
             return arraySoFar;
-
-        }, arraySoFar);
-        return arraySoFar;
-    }, []);
+        }, []);
 
     return returnArray;
+}
+
+function notInArray(array, element){
+    return array.indexOf(element) == -1;
 }
 
 
