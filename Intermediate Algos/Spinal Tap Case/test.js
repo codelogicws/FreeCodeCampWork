b68ff706b33b1f190e3d357f5948d302f315d23c
codelogicws <russ@codelogic.ws>
2016-04-17 22:02:10 -0700

    Improved the "Spinal Tap Case" algo

--- Intermediate Algos/Spinal Tap Case/test.js
+++ Intermediate Algos/Spinal Tap Case/test.js
@@ -1,11 +1,7 @@
 function spinalCase(str) {
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
     str = str.replace(/ |_/g, "-");
-    str = str.replace(/[A-Z]/g, function(x){
-        return "-" + x.toLowerCase();
-    });
-    str = str.replace(/--/g, "-");
     str = str.toLowerCase();
-    str = (str[0] == '-')? str.substring(1) : str;
     return str;
 }
 
