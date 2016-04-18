6559c2184d1bb283da65c620d980086e9f700564
codelogicws <russ@codelogic.ws>
2016-04-17 21:54:50 -0700

    I have a solution that I do not like.

--- Intermediate Algos/Spinal Tap Case/test.js
+++ Intermediate Algos/Spinal Tap Case/test.js
@@ -1,5 +1,19 @@
+function spinalCase(str) {
+    str = str.replace(/ |_/g, "-");
+    str = str.replace(/[A-Z]/g, function(x){
+        return "-" + x.toLowerCase();
+    });
+    str = str.replace(/--/g, "-");
+    str = str.toLowerCase();
+    str = (str[0] == '-')? str.substring(1) : str;
+    return str;
+}
 
 function run(){
+    var test1 = spinalCase('This Is Spinal Tap');
+    var test4 = spinalCase('ThisIsSpinalTap');
+    var test2 = spinalCase("AllThe-small Things");
+    var test3 = spinalCase("The_Andy_Griffith_Show");
     debugger;
 }
 
