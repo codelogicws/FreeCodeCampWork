87554d428cb3ece23b2ee1794edfb2bb551550d2
codelogicws <russ@codelogic.ws>
2016-04-17 21:10:01 -0700

    Finished the "Convert HTML Entities" assignment

--- Intermediate Algos/Convert HTML Entities/test.js
+++ Intermediate Algos/Convert HTML Entities/test.js
@@ -1,11 +1,20 @@
+var replaceTable = {
+    '&amp;': /&/g,
+    '&lt;': /</g,
+    '&gt;': />/g,
+    '&quot;': /"/g,
+    '&apos;': /'/g,
+}
 function convertHTML(str) {
-    // &colon;&rpar;
+    for(var key in replaceTable)
+        str = str.replace(replaceTable[key], key);
     return str;
 }
 
 
 function run(){
     var test1 = convertHTML("Dolce & Gabbana");
+    var test2 = convertHTML("Hamburgers < Pizza < Tacos");
     debugger;
 }
 
