dc28366f25add7390e8346d082c229874d5430ed
codelogicws <russ@codelogic.ws>
2016-04-19 21:43:16 -0700

    Added a bunch of templates for upcoming projects

--- Intermediate Algos/Sum All Primes/test.js
+++ Intermediate Algos/Sum All Primes/test.js
@@ -1,5 +1,17 @@
 function sumPrimes(num) {
-    return num;
+    var sum = 0;
+    for(var i=2;i<=num;i++)
+        if(isPrime(i))
+            sum += i;
+    return sum;
+}
+
+function isPrime(num){
+    var x = Math.sqrt(num);
+    for(var i=2;i<=x;i++)
+        if(num % i === 0)
+            return false;
+    return true;
 }
 
 function run(){
