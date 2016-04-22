ffc06ef67804c87aaf0aaedf7c0038ffd88b2d9e
codelogicws <russ@codelogic.ws>
2016-04-21 20:01:07 -0700

    Solved the "Everything Be True" assignment

--- Intermediate Algos/Everything Be True/test.js
+++ Intermediate Algos/Everything Be True/test.js
@@ -1,5 +1,16 @@
+function truthCheck(collection, pre) {
+    return !collection.reduce(function(notMatch, x){
+        return (notMatch || !(x[pre]))?true : false;
+    }, false);
+}
+
 
 function run(){
+    console.log("test");
+    var test1 = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+    var test3 = truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+    var test2 = test1;
+    var test4 = test3;
     debugger;
 }
 
