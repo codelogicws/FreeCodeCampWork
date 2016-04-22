c0cafdbe567c4639fed8e82351450f76d396d4b6
codelogicws <russ@codelogic.ws>
2016-04-21 21:33:11 -0700

    This feels slightly better for the "Arguments Optional" assignment

--- Intermediate Algos/Arguments Optional/test.js
+++ Intermediate Algos/Arguments Optional/test.js
@@ -1,20 +1,24 @@
 function addTogether() {
     var args = Array.from(arguments);
-    if(typeof(args[0]) !== 'number'){
+
+    if( notNumber(args[0]) ){
         return undefined;
     }else if(args.length == 2){
-        return (typeof(args[1]) !== 'number')?undefined : args[0] + args[1];
+        return ( notNumber(args[1]) )?undefined : args[0] + args[1];
     }else{
+
         return function(c){
-            return (typeof(c) !== 'number')?undefined: args[0] + c;
+            return ( notNumber(c) )?undefined: args[0] + c;
         };
     }
 }
 
+function notNumber(x){
+    return typeof(x) !== 'number';
+}
+
 
 function run(){
-    var test = 5;
-    alert((typeof(test) === 'number'));
     var test1 = addTogether(2,3);
     var test2 = addTogether(2);
     var test3 = test2(3);
