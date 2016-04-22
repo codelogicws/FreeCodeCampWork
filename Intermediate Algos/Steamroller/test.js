553921a1088a6e02c8830b2a4b1f089f61246787
codelogicws <russ@codelogic.ws>
2016-04-21 17:59:35 -0700

    I'm really liking this Steamroller solution. Took me a bit of thinking though

--- Intermediate Algos/Steamroller/test.js
+++ Intermediate Algos/Steamroller/test.js
@@ -1,5 +1,13 @@
+function steamrollArray(arr) {
+    var newArray = arr.reduce(function(arrSoFar, element){
+        var current = (Array.isArray(element))?steamrollArray(element):element;
+        return arrSoFar.concat(current);
+    }, []);
+    return newArray;
+}
 
 function run(){
+    var test = steamrollArray([1, [2], [3, [[4]]]]);
     debugger;
 }
 
