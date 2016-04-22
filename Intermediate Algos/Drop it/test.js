bfa9f9020a73bfba32037ebef62ddbc5a4be8373
codelogicws <russ@codelogic.ws>
2016-04-21 17:30:12 -0700

    Finished Drop it and Finders Keepers

--- Intermediate Algos/Drop it/test.js
+++ Intermediate Algos/Drop it/test.js
@@ -1,5 +1,17 @@
+function dropElements(arr, func) {
+    var keep = false;
+    var retArray = arr.filter(function(x){
+        debugger;
+        if(!func(x)){
+            keep = true;
+        }
+        return keep;
+    });
+    return retArray;
+}
 
 function run(){
+    var test2 = dropElements([1, 2, 3, 4], function(n) {return n < 3; });
     debugger;
 }
 
--- Intermediate Algos/Finders Keepers/test.js
+++ Intermediate Algos/Finders Keepers/test.js
@@ -1,5 +1,14 @@
+function findElement(arr, func) {
+    var num = 0;
+    for(var i=0;i<arr.length;i++){
+        if(func(arr[i]))
+            return arr[i];
+    }
+    return undefined;
+}
 
 function run(){
+    var test2 = findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
     debugger;
 }
 
