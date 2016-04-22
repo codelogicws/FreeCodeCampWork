7fa2a2cf7488ddfb3b9bc055e923bcee432c6ae6
codelogicws <russ@codelogic.ws>
2016-04-21 19:20:05 -0700

    Finished Beinary Agents

--- Intermediate Algos/Binary Agents/test.js
+++ Intermediate Algos/Binary Agents/test.js
@@ -1,5 +1,14 @@
+function binaryAgent(str) {
+    var charArray = str.split(' ').map(function(x){
+        return String.fromCharCode(parseInt(x, 2));
+    });
+    return charArray.join('');
+}
+
 
 function run(){
+    var test = binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+    var test2 = test;
     debugger;
 }
 
