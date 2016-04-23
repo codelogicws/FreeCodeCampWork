1f57ed5a427952b2d9eaf881a39b188b9286c825
codelogicws <russ@codelogic.ws>
2016-04-22 23:13:58 -0700

    Added the new Advance Algo skeletons as well as some work on the Telephone project

---
+++ Advance Algos/Validate US Telephone Numbers/test.js
@@ -0,0 +1,19 @@
+function telephoneCheck(str) {
+    // Good luck!
+    var regex = new RegExp(/^[1]*[ ]*[\(]*[\d]{3}[\)]*[ -]*[\d]{3}[ -]*[\d]{4}$/);
+    return regex.test( str);
+
+}
+
+function run(){
+    var test1 = test("5555559548", true);
+    var test2 = test("15895", false);
+    var test3 = test("1(824) 518-8421", true);
+    var test4 = test("1 555-555-5555", true);
+    var test5 = test("1 555)555-5555", false);
+    debugger;
+}
+
+function test(input, booleanShouldBe){
+    return telephoneCheck(input) == booleanShouldBe;
+}
