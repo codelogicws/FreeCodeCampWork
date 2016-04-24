41910694d28480fc55b63f05aa25e9600c4fe0aa
codelogicws <russ@codelogic.ws>
2016-04-24 10:55:36 -0700

    Work done on the "No repeats please" assignment

--- Advance Algos/No repeats please/test.js
+++ Advance Algos/No repeats please/test.js
@@ -1,5 +1,32 @@
+var map;
+function permAlone(str) {
+    map = {};
+    var array = str.split("");
+    return countPerm(array, []);
+}
+
+function countPerm(arrStill, arrDone){
+    var count = 0;
+    for(var i=0;i<arrStill.length;i++){
+        if(arrStill[i] != arrDone[-1] ){
+            var newStill = arrStill.slice(0);
+            var removed = newStill.splice(i, 1);
+            var newDone = arrDone.slice(0);
+            newDone.push(removed[0]);
+            if( !map[newDone] )
+                count += countPerm(newStill, newDone);
+        }
+    }
+
+    if(arrStill.length == 0){
+        map[arrDone] = true;
+        console.log(arrDone);
+    }
+    return (arrStill.length == 0)? 1+count : count;
+}
 
 function run(){
+    var test = permAlone('abcde');
     debugger;
 }
 
