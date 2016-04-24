111e79b30cec4184ded753541413b1a812a498b0
codelogicws <russ@codelogic.ws>
2016-04-24 11:17:10 -0700

    Finished the "No repeats" even if the time complexity is huge :P

--- Advance Algos/No repeats please/test.js
+++ Advance Algos/No repeats please/test.js
@@ -1,6 +1,4 @@
-var map;
 function permAlone(str) {
-    map = {};
     var array = str.split("");
     return countPerm(array, []);
 }
@@ -8,25 +6,19 @@ function permAlone(str) {
 function countPerm(arrStill, arrDone){
     var count = 0;
     for(var i=0;i<arrStill.length;i++){
-        if(arrStill[i] != arrDone[-1] ){
+        if(arrStill[i] != arrDone[arrDone.length - 1] ){
             var newStill = arrStill.slice(0);
             var removed = newStill.splice(i, 1);
             var newDone = arrDone.slice(0);
             newDone.push(removed[0]);
-            if( !map[newDone] )
-                count += countPerm(newStill, newDone);
+            count += countPerm(newStill, newDone);
         }
-    }
-
-    if(arrStill.length == 0){
-        map[arrDone] = true;
-        console.log(arrDone);
     }
     return (arrStill.length == 0)? 1+count : count;
 }
 
 function run(){
-    var test = permAlone('abcde');
-    debugger;
+    var test = permAlone('aab');
+    console.log(test);
 }
 
