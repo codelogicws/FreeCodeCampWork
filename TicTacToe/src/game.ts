a6af46ae1bb8c122ccf771054a41c5c0a44b53b7
codelogicws <russ@codelogic.ws>
2016-05-01 19:17:33 -0700

    I decided to take the projects out of this repo.

--- TicTacToe/src/game.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="board"/>
-var test1 = function(){
-  console.log('hello test');
-};
-
-var board = new Board();
-board.test();
--- TicTacToe/test/GameSpec.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-var umm = require('./../js/tsc');
-
-describe('Piece Placement', function(){
-  it('Should add a piece', function(){
-    umm.test1();
-    expect(true).to.be.true;
-  });
-});
